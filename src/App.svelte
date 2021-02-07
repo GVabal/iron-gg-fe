ff7b64a898949fa90ed2b972b5754e26f91d7184
s031199 <ernestastuas11@gmail.com>
2021-02-07 20:40:59 +0200

    test

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,13 @@
 <script>
-	import { Router, Link, Route } from 'svelte-routing';
+	import { Router, Link, Route, link } from 'svelte-routing';
 	import HelloWorld from './components/HelloWorld.svelte';
 	import HelloMaterialUI from './components/HelloMaterialUI.svelte';
 	import HelloAxios from './components/HelloAxios.svelte';
+	import Header from './feat/header/header.svelte';
 
-	export let url = '';
+	export let url = 'localhost:5000';
 </script>
 
 <Router url="{url}">
-	<nav>
-	  <Link to="/">Home</Link> |
-	  <Link to="axios">Axios</Link> |
-	  <Link to="mui">Material UI</Link>
-	</nav>
-	<div>
-	  <Route path="axios" component="{HelloAxios}" />
-	  <Route path="mui" component="{HelloMaterialUI}" />
-	  <Route path="/"><HelloWorld /></Route>
-	</div>
-  </Router>
+	<Header />
+</Router>
---
+++ src/components/Header.svelte
@@ -0,0 +1,63 @@
+<style>
+    #headerbackground {
+    /* position: absolute; */
+    width:100%;margin:0;
+    height: 128px;
+    left: 0px;
+    top: 0px;
+    background: #9B8C8C;
+    }
+    
+    header {
+    width: 153px;
+    height: 50px;
+    left: 20px;
+    top: 10px;
+    font-family: Paytone One;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 50px;
+    color: #000000;
+    }
+    
+    #symbolnightmode {
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        font-size: 250%;
+    }
+    
+    ul {
+        width: 100%;
+       display: table;
+       text-align: center;
+    }
+    
+    li{
+    width: 110px;
+    height: 50px;
+    top: 64px;
+    font-family: Paytone One;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 50px;
+    display: inline-block;
+    float: none;
+    color: #000000;
+    }
+    
+    </style>
+    
+    
+<nav id=headerbackground>
+  <header>IRON.GG</header>
+    <span id=symbolnightmode >&#9728;</span>
+    <ul>
+                <li><a href="">Tab1</a></li>
+                <li><a href="">Tab2</a></li>
+                <li><a href="">Tab3</a></li>
+                <li><a href="">Tab4</a></li>
+            </ul>
+        </nav>
